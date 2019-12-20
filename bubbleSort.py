dc313c190b56185baa6ce58643b010bfc6ad96d4
TTOEKS <45870951+TTOEKS@users.noreply.github.com>
2019-12-20 19:00:43 +0900

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,6 +1,7 @@
 def bubbleSort(a):
     for last in range(len(a)-1, 0, -1):
         for i in range(0, last):
+            # Compare value on right 
             if(a[i] > a[i+1]):
                 a[i], a[i+1] = a[i+1], a[i]
     return a
